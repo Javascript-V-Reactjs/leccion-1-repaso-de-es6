876e704499c380cfcda2b1d388bd2aaf53f332fd
Jose M Carrillo <josemanuelcarrillo.dev@gmail.com>
2021-09-05 14:17:28 -0600

    chore: delete some unused directories

--- src/leccion1/template-literals.js
+++
@@ -1,8 +0,0 @@
-let brand = 'tesla'
-let model = 'roadster'
-let price = 1000000
-let tax = 0.13
-
-// let message = 'The price of the ' + brand + ' ' + model + ' is $' + (price + price * tax)
-let message = `The price of the ${brand} ${model} is $${price + price * tax}`
-console.log(message)
